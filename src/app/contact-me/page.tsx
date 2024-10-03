a1f71058565d1f6b6f6e6824282bd78a8ad4b68e
Juan Saldana <saldanaj97@gmail.com>
2024-10-03 12:47:20 -0500

    ui updates to the spacing, forms, and colors

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,4 +1,3 @@
-import { Input } from "@nextui-org/input";
 import { Radio, RadioGroup } from "@nextui-org/radio";
 import { AiOutlineMail } from "react-icons/ai";
 import { CiLinkedin } from "react-icons/ci";
@@ -49,24 +48,26 @@ export default function ContactMe() {
   const IconComponent: React.FC<{ icon: IconType | null }> = ({
     icon: Icon,
   }) => {
-    return Icon ? <Icon size={24} className="" /> : null;
+    return Icon ? (
+      <div className="flex flex-col">
+        <Icon size={24} className="" />
+      </div>
+    ) : null;
   };
 
   const Information = () => {
     return (
-      <div className="flex w-2/5 flex-col gap-y-4 rounded-s-xl bg-primary px-12">
+      <div className="flex h-full w-2/5 flex-col gap-y-4 rounded-s-xl bg-white px-12">
         {informationSectionCopywriting.map((section) => (
-          <div className="flex flex-col">
-            <div className="flex flex-row space-x-2">
-              <div className="flex flex-col justify-start">
-                <IconComponent icon={section.icon} />
-              </div>
+          <div className="flex flex-col" key={section.title}>
+            <div className="flex flex-row items-start space-x-2">
+              <IconComponent icon={section.icon} />
               <div className="flex flex-col">
                 <h2 className="text-lg font-bold">{section.title}</h2>
-                <p className="mb-2 text-sm font-normal">
+                <p className="mb-1 text-sm font-normal">
                   {section.description}
                 </p>
-                <p className="text-base font-semibold">{section.value}</p>
+                <p className="text-sm font-semibold">{section.value}</p>
               </div>
             </div>
           </div>
@@ -77,12 +78,11 @@ export default function ContactMe() {
 
   const ContactForm = () => {
     return (
-      <div className="flex h-full w-3/5 flex-col justify-center rounded-r-xl bg-white px-12 py-16">
+      <div className="flex h-full w-3/5 flex-col justify-center rounded-xl bg-primary px-12 py-16">
         <div className="flex flex-col justify-evenly space-y-8">
           {/* Headers */}
           <h1 className="text-4xl font-semibold">
-            Looking for a developer or just need a website built? Contact me
-            below.
+            Looking for a developer or need a website built? Contact me below.
           </h1>
           <h2 className="text-xl">
             Tell me more about yourself or your company and what you have in
@@ -90,24 +90,42 @@ export default function ContactMe() {
           </h2>
 
           {/* Form */}
-          <form className="flex flex-col space-y-4">
-            <Input type="text" placeholder="Name" variant="underlined" />
-            <Input type="email" placeholder="Email" variant="underlined" />
-            <Input type="text" placeholder="Message" variant="underlined" />
-            <RadioGroup label="What can I help you with?">
-              <div className="flex flex-wrap gap-4">
+          <form className="flex flex-col space-y-8">
+            <input
+              type="text"
+              placeholder="Name"
+              className="border-b-2 border-b-black/60 bg-transparent py-1 text-sm placeholder-black/60 focus:border-white focus:outline-none focus:transition-colors focus:duration-300"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="border-b-2 border-b-black/60 bg-transparent py-1 text-sm placeholder-black/60 focus:border-white focus:outline-none focus:transition-colors focus:duration-300"
+            />
+            <input
+              placeholder="Message"
+              type="textarea"
+              className="h-[40px] w-full border-b-2 border-b-black/60 bg-transparent py-1 text-sm placeholder-black/60 focus:border-white focus:outline-none focus:transition-colors focus:duration-300"
+            />
+            <RadioGroup
+              label="What can I help you with?"
+              color="secondary"
+              classNames={{
+                label: "text-foreground text-sm",
+              }}
+            >
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <Radio value="implement-designs">
-                  Implement exisiting design
+                  Implement existing design
                 </Radio>
                 <Radio value="full-time-job">Full-time opportunity</Radio>
                 <Radio value="part-time-job">
-                  Part-time/Intership opportunity
+                  Part-time/Internship opportunity
                 </Radio>
-                <Radio value="other">Something else</Radio>
+                <Radio value="other">Other</Radio>
               </div>
             </RadioGroup>
             <div className="flex justify-center">
-              <button className="rounded-xl bg-primary px-4 py-2 text-white">
+              <button className="rounded-xl bg-white px-4 py-2 text-black">
                 Submit
               </button>
             </div>
@@ -119,7 +137,7 @@ export default function ContactMe() {
 
   return (
     <div className="flex flex-row items-center justify-center">
-      <div className="flex w-4/5 max-w-7xl flex-row items-center justify-center rounded-xl bg-primary shadow-large">
+      <div className="flex w-4/5 max-w-7xl flex-row items-center justify-center rounded-xl">
         <Information />
         <ContactForm />
       </div>
