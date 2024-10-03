d6795b85557d8eb9b3b9ee42e425b3e0b48ae589
Juan Saldana <saldanaj97@gmail.com>
2024-10-03 13:46:32 -0500

    Add maintenance page for the blog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+  return (
+    <div className="flex flex-col justify-center text-center">
+      <h2 className="text-2xl font-semibold">I'm Sorry.</h2>
+      <h2 className="text-lg font-bold">
+        This page is currently under maintainance. Please check back later.
+      </h2>
+    </div>
+  );
+}
