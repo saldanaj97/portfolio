40a275d91e73dad971afd022c746c5a6ce405b90
Juan Saldana <saldanaj97@gmail.com>
2024-10-16 13:01:03 -0500

    improved animations

--- src/app/(landing-page)/skills.tsx
+++ src/app/(landing-page)/skills.tsx
@@ -15,17 +15,32 @@ interface IconComponentProps {
 
 export default function Skills() {
   const SectionHeadings: React.FC = () => (
-    <div className="flex w-1/3 flex-col justify-center space-y-2 text-center">
-      <h1 className="text-2xl font-semibold md:text-5xl">
+    <motion.div className="flex flex-col justify-center space-y-2 text-center">
+      {/* Animate h1 from the top */}
+      <motion.h1
+        className="text-center text-2xl font-semibold md:text-5xl"
+        initial={{ opacity: 0, y: -100 }} // Start off-screen above
+        animate={{ opacity: 1, y: 0 }} // Animate to the center
+        transition={{ delay: 1, duration: 1 }} // Adjust delay for clockwise effect
+        exit={{ opacity: 0, y: -100 }} // Exit off-screen above
+      >
         Experienced Across Multiple Stacks and Technologies
-      </h1>
-      <h2 className="text-small text-gray-500 md:text-2xl">
+      </motion.h1>
+
+      {/* Animate h2 from the bottom */}
+      <motion.h2
+        className="text-center text-small text-gray-500 md:text-2xl"
+        initial={{ opacity: 0, y: 100 }} // Start off-screen below
+        animate={{ opacity: 1, y: 0 }} // Animate to the center
+        transition={{ delay: 1.2, duration: 1 }} // Adjust delay for clockwise effect
+        exit={{ opacity: 0, y: 100 }} // Exit off-screen below
+      >
         I've worked with a variety of languages and frameworks to build
         applications that are scalable, maintainable, and efficient. If I have
         not worked with a particular technology you are looking for, I am always
         eager to learn and expand my skillset.
-      </h2>
-    </div>
+      </motion.h2>
+    </motion.div>
   );
 
   const IconComponent: React.FC<IconComponentProps> = ({
@@ -34,29 +49,33 @@ export default function Skills() {
     count,
   }) => {
     return (
-      <>
-        <motion.div className="flex flex-col items-center justify-center rounded p-4">
-          <Icon size={64} className="text-blue-500" />
-          <h2 className="text-2xl font-bold">{count}</h2>
-          <p>{label}</p>
-        </motion.div>
-      </>
+      <motion.div className="flex flex-col items-center justify-center rounded p-4">
+        <Icon size={128} className="text-blue-500" />
+        <h2 className="text-center text-2xl font-bold">{count}</h2>
+        <p className="text-center">{label}</p>
+      </motion.div>
     );
   };
+
   return (
-    <>
-      <motion.div className="flex h-screen w-full flex-row justify-center gap-8 p-8">
+    <div className="py-16">
+      <motion.div
+        className="flex flex-col md:grid md:grid-cols-[1fr_1fr_1fr] md:gap-4"
+        initial={"hidden"}
+        whileInView={"visible"}
+        viewport={{ once: true }}
+      >
         {/* Left Icon Column */}
-        <motion.div className="flex flex-col justify-between">
+        <motion.div className="flex flex-col items-center justify-between">
           <motion.div
             className="flex"
             initial={{
               opacity: 0,
-              transform: "translateX(-500%) translateY(-100%)",
+              transform: "translateX(-500%) translateY(-100%)", // Translate based off scroll progress
             }}
             animate={{
               opacity: 1,
-              transition: { delay: 0.5, duration: 1 },
+              transition: { delay: 1, duration: 1 },
               transform: "translateY(0%)",
             }}
             exit={{ opacity: 0 }}
@@ -74,7 +93,7 @@ export default function Skills() {
             }}
             animate={{
               opacity: 1,
-              transition: { delay: 0.5, duration: 1 },
+              transition: { delay: 1.5, duration: 1 }, // Delayed for clockwise effect
               transform: "translateY(0%)",
             }}
             exit={{ opacity: 0 }}
@@ -87,7 +106,7 @@ export default function Skills() {
           </motion.div>
         </motion.div>
 
-        {/* Middle Icon Column */}
+        {/* Middle Heading Column */}
         <motion.div
           className="flex flex-col items-center justify-center"
           initial={{
@@ -95,7 +114,7 @@ export default function Skills() {
           }}
           animate={{
             opacity: 1,
-            transition: { delay: 0.5, duration: 1 },
+            transition: { delay: 0.25, duration: 1 }, // Delayed for clockwise effect
             transform: "translateY(0%)",
           }}
           exit={{ opacity: 0 }}
@@ -113,7 +132,7 @@ export default function Skills() {
             }}
             animate={{
               opacity: 1,
-              transition: { delay: 0.5, duration: 1 },
+              transition: { delay: 2, duration: 1 }, // Delayed for clockwise effect
               transform: "translateY(0%)",
             }}
             exit={{ opacity: 0 }}
@@ -131,7 +150,7 @@ export default function Skills() {
             }}
             animate={{
               opacity: 1,
-              transition: { delay: 0.5, duration: 1 },
+              transition: { delay: 2.25, duration: 1 }, // Delayed for clockwise effect
               transform: "translateY(0%)",
             }}
             exit={{ opacity: 0 }}
@@ -144,6 +163,6 @@ export default function Skills() {
           </motion.div>
         </motion.div>
       </motion.div>
-    </>
+    </div>
   );
 }
